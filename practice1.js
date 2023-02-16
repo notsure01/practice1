5c4e0b7e0a651c502d32ba09ed998cdb3c177954
jerry <0112jerrytseng@gamil.com>
2023-02-16 11:32:55 +0800

    change

--- practice1.js
+++ practice1.js
@@ -1,12 +1,32 @@
-function merge(x, y)
-{   var short;
-    var all;
-    if(x.length > y.length){ short = y.length}
-    else{short = x.length}
-    for(var i=0; i<short; i++)
-    {
-        all.push(x[i]);
-        all.push(y[i]);
+    var text1;
+    var text2;
+    function storeString(){
+        
+     text1 = document.getElementById("string1").value;
+     text2 = document.getElementById("string2").value;
+
     }
-    return all;
-}
+
+    function merge(x, y){
+        var longer;
+        var all = "";
+        var array1 = [...x];
+        var array2 = [...y];
+
+        if(array1.length > array2.length)  // 找出較長的字串
+        { longer = array1.length;}
+        else
+        { longer = array2.length;}
+
+        for(var i=0; i<longer; i++)
+        {   
+            if(i < array1.length)
+            {all += array1[i];}
+            if(i < array2.length)
+            {all += array2[i];}
+        
+        }
+
+        document.getElementById("demo").innerHTML = all;
+        return all;
+    }
